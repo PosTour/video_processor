ae9ad415b49699d4f8bbb8356337f6bcb70d357f
posTour <dimidov496@gmail.com>
2025-02-15 22:28:05 +0300

    feat: модель для хранения ответов

---
+++ src/main/java/ru/bmstu/video_processor/domain/FrameResponse.java
@@ -0,0 +1,44 @@
+package ru.bmstu.video_processor.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "response")
+@NoArgsConstructor
+public class FrameResponse {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "value1")
+    private double value1;
+
+    @Column(name = "value2")
+    private double value2;
+
+    @Column(name = "value3")
+    private double value3;
+
+    @Column(name = "value4")
+    private double value4;
+
+    @Column(name = "text")
+    private String text;
+
+    public FrameResponse(double value1, double value2, double value3, double value4, String text) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+        this.value4 = value4;
+        this.text = text;
+    }
+}
